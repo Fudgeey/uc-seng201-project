20a4e95178456426efeb81bc257920470705bb93
rileyjeffcote <rje90@uclive.ac.nz>
2024-04-29 11:31:00 +1200

    shop screen design implemented and changing between screens.

---
+++ src/main/java/seng201/team43/services/ShopService.java
@@ -0,0 +1,13 @@
+package seng201.team43.services;
+
+import seng201.team43.models.GameManager;
+
+public class ShopService {
+    private final GameManager gameManager;
+
+
+    public ShopService(GameManager gameManager) {
+        this.gameManager = gameManager;
+    }
+
+}
